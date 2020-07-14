90448ce26ba42053dbec3e892247e5a7ccbdf405
HoKoung Kim <ghrud24020@naver,com>
2020-07-14 17:41:13 +0900

    함수만들기

---
+++ print_star_plus.py
@@ -0,0 +1,10 @@
+def print_star_plus() :
+    print('++++++++++++++++++++++++++++++++')
+
+def print_star3() :
+    print('****************************')
+
+print_star_plus()
+print_star3()
+print_star_plus()
+print_star3()
